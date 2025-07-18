64f3d1345ecd6b2ccb1233ed5a1300f1464922df
Horman <rateevk@mail.ru>
2025-07-18 10:56:22 +0500

    make some edits new

--- handlers/answer_to_user.py
+++ handlers/answer_to_user.py
@@ -15,7 +15,7 @@ async def reply_handler(message: Message):
         print(id_user)
         await config.bot.send_message(chat_id=id_user, text=message.text)
         await message.reply(f"Ответ отправлен!")
-    elif 'Ответ отправлен!' in m or 'Ожидайте ответа' in m:
+    elif 'Ответ отправлен!' in m or 'Ожидайте ответа' in m or 'Скрипт тех поддержки запущен в данном чате' in m:
         await message.reply('Возможно вы ответили на системное сообщение, повторите попытку отвечая не на системное сообщение')
     else:
         text = message.text
